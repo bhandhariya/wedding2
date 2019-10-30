d744493e804e3157f62078839ce64a40596f9305
raja <sainiraja59@gmail.com>
2019-10-30 12:02:47 +0530

    gdsjjgf

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ var path=require('path')
 app.listen(8080,function(){console.log("server readt on 1234")});
 // app.use('/',express.static('dist/aiproj'))
 app.use('/',express.static('dist/aiproj'))
+
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/aiproj/index.html'));
+    });
 app.use('/app',express.static('public'))
 
