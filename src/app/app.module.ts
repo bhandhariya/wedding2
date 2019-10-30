917d3f00537943ee9299c188a51bdc5c76effeb4
raja <sainiraja59@gmail.com>
2019-10-30 18:37:58 +0530

    first commit in this branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { StoryComponent } from './shared/story/story.component';
+import { TimerComponent } from './shared/timer/timer.component';
+import { JourneyComponent } from './shared/journey/journey.component';
+import { MomentComponent } from './shared/moment/moment.component';
+import { ReserveComponent } from './shared/reserve/reserve.component';
+import { ProgressComponent } from './shared/progress/progress.component';
+import { ClientComponent } from './shared/client/client.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    StoryComponent,
+    TimerComponent,
+    JourneyComponent,
+    MomentComponent,
+    ReserveComponent,
+    ProgressComponent,
+    ClientComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
