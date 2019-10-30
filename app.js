cea106fadc7076f1523e764a0e136ee3eac9b85a
raja <sainiraja59@gmail.com>
2019-10-30 12:09:16 +0530

    sadsdas

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var express=require('express');
-var app=express();
-var path=require('path')
-app.listen(8080,function(){console.log("server readt on 1234")});
+// var express=require('express');
+// var app=express();
+// var path=require('path')
+// app.listen(8080,function(){console.log("server readt on 1234")});
+// // app.use('/',express.static('dist/aiproj'))
 // app.use('/',express.static('dist/aiproj'))
-app.use('/',express.static('dist/aiproj'))
+
+// app.get('/*', function(req,res) {
+    
+//     res.sendFile(path.join(__dirname+'/dist/aiproj/index.html'));
+//     });
+// app.use('/app',express.static('public'))
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/aiproj'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/dist/aiproj/index.html'));
-    });
-app.use('/app',express.static('public'))
+res.sendFile(path.join(__dirname+'/dist/aiproj/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
